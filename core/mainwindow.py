17cb54222f4d5f690396351faa277f022bc68bbb
wanghz <h8d9123@163.com>
2017-12-01 20:44:00 +0800

    implemnts conncetAct, and set mplwidget

--- core/mainwindow.py
+++ core/mainwindow.py
@@ -2,14 +2,21 @@
 import sys,os
 from PyQt4 import QtGui,Qt,QtCore
 import mplcanvas
+import wr34dialogs
+import connectdialog
+import logging
+from resource import icon_rc,icons
 class MainWindow(QtGui.QMainWindow):
     def __init__(self,parent = None):
         super(MainWindow, self).__init__(parent)
+        self.connector = None
         self.setupUi()
+        self.connectInit()
     def setupUi(self):
         
         #设置中部显示区
         self.createMenu()
+        self.createToolBar()
         self.centralWidget = QtGui.QFrame(self)
         self.centralWidget.setFrameStyle(QtGui.QFrame.Box|QtGui.QFrame.Raised)
         self.centralWidget.setLineWidth(1)
@@ -24,9 +31,12 @@ class MainWindow(QtGui.QMainWindow):
         frame1.setFrameStyle(QtGui.QFrame.Box|QtGui.QFrame.Raised)
         frame1.setLineWidth(1)
         self.dockWidgetDown.setWidget(frame1)
-        hbox_state = QtGui.QHBoxLayout(frame1)
-        self.stateDialog = QtGui.QDialog()
-        hbox_state.addWidget(self.stateDialog)
+        vbox_state = QtGui.QVBoxLayout(frame1)
+        self.tabWnd = QtGui.QTabWidget()
+        self.specWnd = QtGui.QTextEdit()
+        self.tabWnd.addTab(self.specWnd, "Spec")
+        vbox_state.addWidget(self.tabWnd)
+        
         self.dockWidgetDown.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
         self.addDockWidget(QtCore.Qt.DockWidgetArea(QtCore.Qt.BottomDockWidgetArea), self.dockWidgetDown)
         
@@ -36,9 +46,10 @@ class MainWindow(QtGui.QMainWindow):
         frame1.setFrameStyle(QtGui.QFrame.Box|QtGui.QFrame.Raised)
         frame1.setLineWidth(1)
         self.dockWidgetRight.setWidget(frame1)
-        hbox_control = QtGui.QHBoxLayout(frame1)
-        self.controlDialog = QtGui.QDialog()
-        hbox_control.addWidget(self.controlDialog)
+        vbox_control = QtGui.QVBoxLayout(frame1)
+        self.controlDialog = wr34dialogs.QWR34SaveDialog(self)
+        vbox_control.addWidget(self.controlDialog)
+        vbox_control.addStretch()
         self.dockWidgetDown.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
         self.addDockWidget(QtCore.Qt.DockWidgetArea(QtCore.Qt.RightDockWidgetArea), self.dockWidgetRight)
         
@@ -47,23 +58,102 @@ class MainWindow(QtGui.QMainWindow):
         self.createAction()
         self.filemenu = self.menuBar().addMenu('&File')
         self.toolMenu = self.menuBar().addMenu('&Tool')
+        self.graphMenu = self.menuBar().addMenu('Graph')
         
-        acts = [self.openAct, self.SaveAct]
+        acts = [self.actOpen, self.actSave,self.actConnect, self.actDisConnect]
         for act in acts:
             self.filemenu.addAction(act)
     
-        acts = [self.plotAct, self.showSpec]
+        acts = [self.actPlot]
         for act in acts:
             self.toolMenu.addAction(act)
+        acts = [self.homeAct,self.backAct, self.upAct, self.downAct, self.nextAct, self.zoomInAct, 
+                self.zoomOutAct,self.lineAct,self.savePicAct, self.panAct]
+        for act in acts:
+            self.graphMenu.addAction(act)
         pass
+    
     def createAction(self):
-        self.openAct = QtGui.QAction('&Open', self)
-        self.SaveAct = QtGui.QAction('&Save', self)
+        self.actOpen = QtGui.QAction(QtGui.QIcon(':/images/open.png'),'&Open', self,triggered=self.onActOpenTriggered)
+        self.actSave = QtGui.QAction(QtGui.QIcon(':/images/save.png'),'&Save', self,triggered=self.onActSaveTriggered)
         
-        self.plotAct = QtGui.QAction('&Plot', self)
+        self.actPlot = QtGui.QAction(QtGui.QIcon(":/icon/Graph.png"),'&Plot', self,triggered=self.onActPlotTriggered)
+        self.backAct = QtGui.QAction(QtGui.QIcon(":/icon/Back.png"), '&Back',self)
+        self.upAct = QtGui.QAction(QtGui.QIcon(":/icon/Up.png"), '&Up',self)
+        self.downAct = QtGui.QAction(QtGui.QIcon(":/icon/Down.png"), '&Down',self)
+        self.nextAct = QtGui.QAction(QtGui.QIcon(":/icon/Next.png"), '&Next',self)
+        self.zoomInAct = QtGui.QAction(QtGui.QIcon(":/icon/Zoom_In.png"), '&Zoom In',self)
+        self.zoomOutAct = QtGui.QAction(QtGui.QIcon(":/icon/Zoom_Out.png"), '&Zoom Out',self)
         
-        self.showSpec = QtGui.QAction('Show Spec',self)
+        self.homeAct = QtGui.QAction(QtGui.QIcon(":/icon/home.png"), '&Home',self)
+        self.borderAct = QtGui.QAction(QtGui.QIcon(":/icon/border.png"), '&Range',self)
+        self.lineAct = QtGui.QAction(QtGui.QIcon(":/icon/line.png"), '&Lines',self)
+        self.savePicAct = QtGui.QAction(QtGui.QIcon(":/icon/savepic.png"), '&Save Picture',self)
+        self.panAct = QtGui.QAction(QtGui.QIcon(":/icon/pan.png"), '&Pan',self)
+        
+        self.actShowSpec = QtGui.QAction('Show Spec',self,triggered=self.onActShowSpecTriggered)
+        self.actConnect = QtGui.QAction(QtGui.QIcon(':/images/connect.png'),'Connect',self,triggered=self.onActConnectTriggered)
+        self.actDisConnect = QtGui.QAction(QtGui.QIcon(':/images/disconnect.png'),'Disconnect',self,triggered=self.onActDisconnectTriggered)
         pass
+    def createToolBar(self):
+        fileBar = self.addToolBar("File")
+        toolBar = self.addToolBar("Tool")
+        graphBar = self.addToolBar("Graph")
+        acts = [self.actOpen, self.actSave,self.actConnect, self.actDisConnect]
+        for act in acts:
+            fileBar.addAction(act)
+        acts = [self.actPlot]
+        for act in acts:
+            toolBar.addAction(act)
+        acts = [self.homeAct,self.backAct, self.upAct, self.downAct, self.nextAct, self.zoomInAct, 
+                self.zoomOutAct,self.lineAct,self.savePicAct, self.panAct]
+        for act in acts:
+            graphBar.addAction(act)
+        pass
+    
+    def connectInit(self):
+        toolbar = self.mplwidget.toolbar
+        self.zoomInAct.triggered.connect(toolbar.zoom)
+        self.backAct.triggered.connect(toolbar.back)
+        self.nextAct.triggered.connect(toolbar.forward)
+        self.homeAct.triggered.connect(toolbar.home)
+        self.borderAct.triggered.connect(toolbar.configure_subplots)
+        self.savePicAct.triggered.connect(toolbar.save_figure)
+        self.panAct.triggered.connect(toolbar.pan)
+        self.lineAct.triggered.connect(toolbar.edit_parameters)
+    def onActOpenTriggered(self):
+        pass
+    
+    def onActSaveTriggered(self):
+        pass
+    
+    def onActPlotTriggered(self):
+        dlg = wr34dialogs.QWR34PlotDialog(self)
+        dlg.show()
+        dlg.exec_()
+        
+    def getAxes(self):
+        return self.mplwidget.axes
+    
+    def getConnector(self):
+        return self.connector
+    
+    def onActShowSpecTriggered(self):
+        pass
+    
+    def onActConnectTriggered(self):
+        ipConfigDlg = connectdialog.QIPConfigDialog(self)
+        ipConfigDlg.setModal(True)
+        ipConfigDlg.show()
+        if ipConfigDlg.exec_() == QtGui.QDialog.Accepted:
+            self.connector = ipConfigDlg.getConnector()
+            
+    def onActDisconnectTriggered(self):
+        # disconnect from an instrument
+        if self.connector:
+            self.connector.close()
+            logging.info('disconnected the instrument')
+    
     
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
